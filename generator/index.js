2f0c3e946e18222beb5fdee2bbbc87baf1dc250b
Patrick Santos <patrickspteixeira@gmail.com>
2020-03-05 20:14:37 -0300

    remove default dirs

--- generator/index.js
+++ generator/index.js
@@ -1,3 +1,15 @@
 module.exports = (api, { preset = 'base' }) => {
     api.render(`./template/${preset}`);
+
+    api.postProcessFiles(files => {
+        console.log('files:', files)
+        Object.keys(files).forEach(name => {
+            if (/^src\/views[/$]/.test(name)) {
+                delete files[name]
+            }
+            if (/^src\/router[/$]/.test(name)) {
+                delete files[name]
+            }
+        })
+    });
 }
