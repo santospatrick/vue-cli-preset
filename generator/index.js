f85992c6d6d12c7a4e741d5ed53747114d6a7022
Patrick Santos <patrickspteixeira@gmail.com>
2020-03-05 20:18:43 -0300

    remove console

--- generator/index.js
+++ generator/index.js
@@ -2,7 +2,6 @@ module.exports = (api, { preset = 'base' }) => {
     api.render(`./template/${preset}`);
 
     api.postProcessFiles(files => {
-        console.log('files:', files)
         Object.keys(files).forEach(name => {
             if (/^src\/views[/$]/.test(name)) {
                 delete files[name]
