362bf4377a37e41cb011d3c229362f05b85bcc44
Patrick Santos <patrickspteixeira@gmail.com>
2020-03-06 18:46:36 -0300

    add form example on admin template

--- generator/template/admin/src/router.js
+++ generator/template/admin/src/router.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from './pages/Home.vue';
+import Login from './pages/Login.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'Login',
+    component: Login,
   },
   {
     path: '/about',
