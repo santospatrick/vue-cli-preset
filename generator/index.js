5b835fc0ead7ccf27ecbd493fa13d6e1051eac0d
Patrick Santos <patrickspteixeira@gmail.com>
2020-03-09 16:01:13 -0300

    add git hooks and App.vue template

--- generator/index.js
+++ generator/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 module.exports = (api, { preset = 'base' }) => {
     api.render(`./template/${preset}`);
 
@@ -9,12 +11,32 @@ module.exports = (api, { preset = 'base' }) => {
             if (/^src\/router[/$]/.test(name)) {
                 delete files[name]
             }
+
+            const appTemplateDirectory = `${__dirname}/template/${preset}/src/App.vue`;
+
+            if (!fs.existsSync(appTemplateDirectory)) return;
+
+            const appTemplate = fs.readFileSync(appTemplateDirectory, 'utf8');
+
+            files['src/App.vue'] = appTemplate;
         })
     });
 
     api.extendPackage({
         dependencies: {
             axios: "^0.19.2"
+        },
+        devDependencies: {
+            "lint-staged": "^10.0.8"
+        },
+        gitHooks: {
+            "pre-commit": "lint-staged"
+        },
+        "lint-staged": {
+            "*.{js,vue}": [
+                "vue-cli-service lint",
+                "git add"
+            ]
         }
     })
 }
---
+++ generator/template/admin/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Login</router-link> |
+      <router-link to="/users">Users</router-link>
+    </div>
+    <v-app>
+      <router-view/>
+    </v-app>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
