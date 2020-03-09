e0cad5e3553d2ed354ee13d3f2ec85d5f6c24de9
Patrick Santos <patrickspteixeira@gmail.com>
2020-03-09 18:23:10 -0300

    update user form state

--- generator/template/admin/src/forms/User.vue
+++ generator/template/admin/src/forms/User.vue
@@ -8,8 +8,34 @@
         >
           <v-text-field
             v-model="data.name"
-            :rules="nameRules"
+            :rules="rules"
             label="Nome"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="data.email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="data.company.name"
+            :rules="rules"
+            label="Empresa"
             required
           ></v-text-field>
         </v-col>
@@ -53,9 +79,18 @@ export default {
       valid: false,
       data: {
         name: this.user.name,
+        email: this.user.email,
+        company: {
+          // Optional Chaining: https://babeljs.io/docs/en/babel-plugin-proposal-optional-chaining
+          name: this.user.company?.name,
+        },
       },
-      nameRules: [
+      rules: [
         (v) => !!v || 'Campo obrigatório',
+      ],
+      emailRules: [
+        (v) => !!v || 'Campo obrigatório',
+        (v) => /.+@.+/.test(v) || 'E-mail inválido',
       ],
     };
   },
