8e78f1a43dbca6396c0c414b2af43e798ba9943b
Patrick Santos <patrickspteixeira@gmail.com>
2020-03-09 18:08:59 -0300

    add delete

--- generator/template/admin/src/pages/users/Index.vue
+++ generator/template/admin/src/pages/users/Index.vue
@@ -20,7 +20,18 @@
             <tbody>
               <tr v-for="item in list" :key="item.name">
                 <td>{{ item.name }}</td>
-                <td><router-link :to="`/users/${item.id}`">Editar usuário</router-link></td>
+                <td>
+                  <router-link :to="`/users/${item.id}`" v-slot="{ navigate }">
+                    <v-btn @click="navigate" text icon>
+                      <!-- All icons: https://github.com/vuetifyjs/vuetify/blob/master/packages/vuetify/src/services/icons/presets/mdi.ts -->
+                      <v-icon>mdi-pencil</v-icon>
+                    </v-btn>
+                  </router-link>
+                  <v-btn @click="deleteUser(item)" text icon>
+                    <!-- All icons: https://github.com/vuetifyjs/vuetify/blob/master/packages/vuetify/src/services/icons/presets/mdi.ts -->
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                </td>
               </tr>
             </tbody>
           </template>
@@ -47,6 +58,13 @@ export default {
     async getUsers() {
       const response = await api.get('/users');
       this.list = response.data;
+    },
+    async deleteUser({ name, id }) {
+      // eslint-disable-next-line
+      if (window.confirm(`Deseja deletar "${name}"?`)) {
+        await api.delete(`/users/${id}`);
+        this.list = this.list.filter((item) => item.id !== id);
+      }
     },
   },
 };
